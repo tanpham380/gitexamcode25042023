a9f2a335c3f3992e0cc0d2afc59f7fae3b9b327d
tanpham380 <phamthanhtanlop92@gmail.com>
2023-05-09 17:49:35 +0700

    Test Sync

--- first_app.dart
+++ first_app.dart
@@ -1,3 +1,15 @@
 void main() {
   print('Hello, world!');
+  int a = 2020;
+  String str = "string value";
+  num b = 3.14;
+  print(a.toString());
+  print("\u023A");
+  var s = "haha";
+  print(s);
+  dynamic d = "assd";
+  // null safety
+  int? c = null;
+  String? S = null;
+  
 }
